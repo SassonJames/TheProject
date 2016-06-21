3c9addffd0d2d512253b59e1e866b863bb193cbb
James Michael Sasson <sassonjames@gmail.com>
2016-06-21 17:30:29 -0400

    Initial Unity Project and Character Movement
    This is the Unity Project that we will be using for the game. There is
    currently a test scene with a Skybox, Floor, Ramps, Stairs, and the
    Player character. The player character has a script that allows them to
    move using WASD and jump using the "spacebar".

---
+++ New Unity Project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    private bool isWalking;
+    public bool isJumping;
+    public bool grounded;
+    public float airTime;
+
+    public bool walkingUp;
+    public bool walkingDown;
+    public bool walkingLeft;
+    public bool walkingRight;
+
+    // Use this for initialization
+    void Start () {
+        walkingUp = false;
+        walkingDown = false;
+        walkingLeft = false;
+        walkingRight = false;
+        isWalking = false;
+        isJumping = false;
+        airTime = 0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Walk();
+        Jump();
+	}
+
+    void Walk()
+    {
+        if (Input.GetKeyDown("w") && walkingUp == false)
+        {
+            walkingUp = true;
+            isWalking = true;
+        }
+        if (Input.GetKeyDown("a") && walkingLeft == false)
+        {
+            walkingLeft = true;
+            isWalking = true;
+        }
+        if (Input.GetKeyDown("s") && walkingDown == false)
+        {
+            walkingDown = true;
+            isWalking = true;
+        }
+        if (Input.GetKeyDown("d") && walkingRight == false)
+        {
+            walkingRight = true;
+            isWalking = true;
+        }
+
+        if(isWalking == true)
+        {
+            if (walkingUp == true)
+            {
+                if( Input.GetKeyUp("w") != true)
+                {
+                    this.transform.position += new Vector3(0f, 0f, 0.075f);
+                }
+                else { walkingUp = false; }
+            }
+            if (walkingLeft == true)
+            {
+                if (Input.GetKeyUp("a") != true)
+                {
+                    this.transform.position += new Vector3(-0.075f, 0f, 0f);
+                }
+                else { walkingLeft = false; }
+            }
+            if (walkingDown == true)
+            {
+                if (Input.GetKeyUp("s") != true)
+                {
+                    this.transform.position += new Vector3(0f, 0f, -0.075f);
+                }
+                else { walkingDown = false; }
+            }
+            if (walkingRight == true)
+            {
+                if (Input.GetKeyUp("d") != true)
+                {
+                    this.transform.position += new Vector3(0.075f, 0f, 0f);
+                }
+                else { walkingRight = false; }
+            }
+            if(walkingUp != true && walkingLeft != true && walkingRight != true && walkingDown != true)
+            {
+                isWalking = false;
+            }
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown("space") && grounded == true)
+        {
+            isJumping = true;
+            grounded = false;
+        }
+        if(isJumping == true)
+        {
+            if (airTime <= 0.15f && Input.GetKeyUp("space") != true)
+            {
+                this.transform.Translate(Vector3.up * 9 * Time.deltaTime, Space.World);
+                airTime += Time.deltaTime;
+            }
+            else if (airTime <= 0.3 && Input.GetKeyUp("space") != true)
+            {
+                this.transform.Translate(Vector3.up * 7 * Time.deltaTime, Space.World);
+                airTime += Time.deltaTime;
+            }
+            else if (airTime <= 0.45 && Input.GetKeyUp("space") != true)
+            {
+                this.transform.Translate(Vector3.up * 4 * Time.deltaTime, Space.World);
+                airTime += Time.deltaTime;
+            }
+            else
+            {
+                airTime = 0;
+                isJumping = false;
+            }
+        }
+    }
+    void OnCollisionEnter (Collision col)
+    {
+        if(col.gameObject.tag == "Floor")
+        {
+            grounded = true;
+        }
+    }
+}
