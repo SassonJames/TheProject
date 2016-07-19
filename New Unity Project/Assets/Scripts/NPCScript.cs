18a7805b7d911ca7796ed8c02bd02646b731722e
James Michael Sasson <sassonjames@gmail.com>
2016-07-19 13:05:50 -0400

    Added NPC Interaction Skeleton
    Added a script and an NPC character to the scene. The player can move
    around the NPC and the NPC knows when the player is within interaction
    distance

---
+++ New Unity Project/Assets/Scripts/NPCScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Entered");
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Exited");
+        }
+    }
+}
