74ed3d75d63a0f393e64c8db3fea52a689246ec7
Richard Kalvaitis <rkalvait@emich.edu>
2016-08-03 14:45:55 -0400

    removed more herobrine. Added building script

--- New Unity Project/Assets/Scripts/PlayerMove.cs
+++ New Unity Project/Assets/Scripts/PlayerMove.cs
@@ -6,31 +6,35 @@ public class PlayerMove : MonoBehaviour {
 	public float speed;
 
     public bool isJumping;
+    public bool isWalking;
     public bool grounded;
+    public bool insideBuilding;
+
     public float airTime;
 	
-	private bool isWalking;
 	private Rigidbody rb;
 
     // Use this for initialization
     void Start () {
-        isWalking = false;
+        isWalking = true;
         isJumping = false;
+        insideBuilding = false;
         airTime = 0f;
 		rb = GetComponent<Rigidbody> ();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Walk();
+        if (isWalking == true)
+        {
+            Walk();
+            Jump();
+        }
         Ground();
-        Jump();
 	}
 
     void Walk()
     {
-		//transform.Translate(new Vector3(Input.GetAxis ("Horizontal")*speed, 0f, Input.GetAxis("Vertical")*speed));
-
 		//transform.Translate(new Vector3(Input.GetAxis ("Horizontal")*speed, 0f, Input.GetAxis("Vertical")*speed));
 		//transform.position += new Vector3(Input.GetAxis ("Horizontal")*speed, 0f, Input.GetAxis("Vertical")*speed);
 		//rb.velocity = new Vector3(Input.GetAxis ("Horizontal")*speed, rb.velocity.y, Input.GetAxis("Vertical")*speed);
@@ -43,7 +47,6 @@ public class PlayerMove : MonoBehaviour {
         {
             transform.position += new Vector3(Input.GetAxis("Vertical") * speed * -1, 0f, Input.GetAxis("Horizontal") * speed);
         }
-		transform.position += new Vector3(Input.GetAxis ("Horizontal")*speed, 0f, Input.GetAxis("Vertical")*speed);
 
     }
     void Jump()
@@ -88,6 +91,12 @@ public class PlayerMove : MonoBehaviour {
         {
             grounded = false;
         }
+    }
+
+    public void RotateCharacter()
+    {
+        this.transform.LookAt(GameObject.FindGameObjectWithTag("Camera").transform.position);
+        this.transform.rotation = new Quaternion(0, this.transform.rotation.y, 0, this.transform.rotation.w);
     }
 
     void OnCollisionEnter (Collision col)
