dbf3593394d6b0ed05613420bf7c248245158d0f
rkalvait <rkalvait@emich.edu>
2016-10-08 01:11:48 -0400

    Finished the puzzle, organized scripts

---
+++ New Unity Project/Assets/Scripts/Environment Scripts/DestroyPair.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyPair : MonoBehaviour {
+
+	public GameObject destroy;
+
+	void OnTriggerEnter(Collider coll) {
+		//Debug.Log (coll.gameObject.name);
+		if (coll.gameObject.tag == "DamageSpell") {
+			//GameManager.audio.PlayOneShot(GameManager.negate);
+			Destroy (destroy);
+			Destroy (coll.gameObject);
+			Destroy (this.gameObject);
+		}
+	}
+}
